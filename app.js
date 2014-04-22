ab1390c0bbf9af5c51efe887a07a1559c29bb70a
Alexander Seliverstov <aseliverstov@smartplaces.ru>
2014-04-22 14:32:24 +0400

    Move Passbook Web Service to standalone file pass.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var restify = require('restify');
 var mongojs = require('mongojs');
 var _ = require('underscore');
-var createTemplate = require("passbook");
 
 var port = '8081';
 
@@ -21,80 +20,10 @@ var messages = db.collection('messages');
 var requests = db.collection('requests');
 
 server.get({path:'/messagefor/:uuid/:major/:minor/:proximity/:event',version:'0.0.1'}, messageFor);
-//Registering a Device to Receive Push Notifications for a Pass
-server.post({path:'/passws/:version/devices/:device_id/registrations/:pass_type_id/:pass_serial'},registerPass);
-//Getting the Serial Numbers for Passes Associated with a Device
-server.get({path:'/passws/:version/devices/:device_id/registrations/:pass_type_id'},getPassSerialNumbers);
-//Getting the Latest Version of a Pass
-server.get({path:'/passws/:version/passes/:pass_type_id/:pass_serial'},getPassLatVersion);
-//Unregistering a Device
-server.del({path:'/passws/:version/devices/:device_id/registrations/:pass_type_id/:pass_serial'},unregisterPass);
-//Logging Errors
-server.post({path:'passws/:version/log'},logPassErrors);
 
 server.listen(port,function(){
   console.log('%s listening at %s',server.name,server.url);
 });
-
-
-
-function registerPass(req, res, next){
-  var pushToken = req.params.pushToken;
-  var authenticationToken = req.header('Authorization');
-  if (authenticationToken) authenticationToken = authenticationToken.replace('ApplePass ','');
-  var version = req.params.version;
-  var device = req.params.device_id;
-  var passTypeId = req.params.pass_type_id;
-  var passSerial = req.params.pass_serial;
-  console.log('registration:');
-  console.log([pushToken,authenticationToken,version,device,passTypeId,passSerial]);
-  res.status(200);
-  return next();
-}
-
-function getPassSerialNumbers(req, res, next){
-  var passesUpdatedSince = req.get('passesUpdatedSince');
-  var version = req.params.version;
-  var device = req.params.device_id;
-  var passTypeId = req.params.pass_type_id;
-  console.log('serial numbers:');
-  console.log([passesUpdatedSince,version,device,passTypeId]);
-  res.status(204);
-  return next();
-}
-
-function getPassLatVersion(req, res, next){
-  var authenticationToken = req.header('Authorization');
-  if (authenticationToken) authenticationToken = authenticationToken.replace('ApplePass ','');
-  var version = req.params.version;
-  var passTypeId = req.params.pass_type_id;
-  var passSerial = req.params.pass_serial;
-  console.log('last version:');
-  console.log([authenticationToken,version,passTypeId,passSerial]);
-  res.status(304);
-  return next();
-}
-
-function unregisterPass(req, res, next){
-  var authenticationToken = req.header('Authorization');
-  if (authenticationToken) authenticationToken = authenticationToken.replace('ApplePass ','');
-  var version = req.params.version;
-  var device = req.params.device_id;
-  var passTypeId = req.params.pass_type_id;
-  var passSerial = req.params.pass_serial;
-  console.log('unregistration:');
-  console.log([authenticationToken,version,device,passTypeId,passSerial]);
-  res.status(200);
-  return next();
-}
-
-function logPassErrors(req,res,next){
-  var log = req.params.logs;
-  console.log('log');
-  console.log(log);
-  res.status(200);
-  return next();
-}
 
 function messageFor(req, res, next){
   res.setHeader('Access-Control-Allow-Origin','*');
---
+++ pass.js
@@ -0,0 +1,58 @@
+var restify = require('restify');
+var _ = require('underscore');
+
+var port = '80';
+
+var server = restify.createServer({
+  name:'restplatform'
+});
+
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.authorizationParser());
+server.use(restify.dateParser());
+server.use(restify.queryParser());
+server.use(restify.jsonp());
+server.use(restify.gzipResponse());
+server.use(restify.bodyParser());
+
+///
+server.post({path:'/passws/v1/devices/:device_id/registrations/:pass_type_id/:serial_number'},function (req, res, next){
+  console.log('Handling registration request...');
+  console.log(
+    "#<RegistrationRequest device_id: " + req.params.device_id +
+    ", pass_type_id: " + req.params.pass_type_id +
+    ", serial_number: " + req.params.serial_number +
+    ", authentication_token: " + req.header('Authorization') +
+    ", push_token: " + req.params.pushToken+">");
+  res.status(200);
+  return next();
+});
+
+server.get({path:'/passws/v1/devices/:device_id/registrations/:pass_type_id?'},function (req, res, next){
+  console.log('Handling updates request...');
+  res.status(204);
+  return next();
+});
+
+server.del({path:'/passws/v1/devices/:device_id/registrations/:pass_type_id/:serial_number'},function (req, res, next){
+  console.log('Handling unregistration request...')
+  res.status(200);
+  return next();
+});
+
+server.get({path:'/passws/v1/passes/:pass_type_id/:serial_number'},function (req, res, next){
+  console.log('Handling pass delivery request...');
+  res.status(401);
+  return next();
+});
+
+server.post({path:'passws/v1/log'},function (req, res, next){
+  console.log('Handling log request...');
+  console.log('#<LogRequest logs: 'req.params.logs+">");
+  res.status(200);
+  return next();
+});
+
+server.listen(port,function(){
+  console.log('%s listening at %s',server.name,server.url);
+});
