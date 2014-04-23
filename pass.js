f75c4377e29f1aabfe66720953cbacbccd49bf55
Alexander Seliverstov <aseliverstov@smartplaces.ru>
2014-04-23 17:38:47 +0400

    Pass registration function - not tested

--- pass.js
+++ pass.js
@@ -1,10 +1,14 @@
 var restify = require('restify');
 var _ = require('underscore');
 var fs = require('fs');
+var mongojs = require('mongojs');
+
+var mongoConnection = 'localhost/smartplaces';
+var db = mongojs(mongoConnection,['smartplaces']);
+var passes = db.collection('passes');
 
 var http_port = '80';
 var https_port = '443';
-
 
 var http_server = restify.createServer({
   name:'http_restplatform'
@@ -46,6 +50,34 @@ function initServer(server){
   });
 
   server.post({path:'/passws/v1/devices/:device_id/registrations/:pass_type_id/:serial_number'},function (req, res, next){
+    var authToken = req.header('Authorization');
+    var serialNumber = req.params.serial_number;
+    var passType = req.params.pass_type_id;
+    var deviceId = req.params.device_id;
+    var pushToken = req.params.pushToken;
+
+    passes.findOne({passType:passType,serialNumber:serialNumber,authToken:authToken}, function (err,pass){
+      if (pass){
+        if (_.indexOf(pass.registrations,deviceId) > -1){
+          res.status(200);
+          return next();
+        }else{
+          passes.update({_id:pass._id},{$addToSet:{registrations:{deviceId:deviceId,pushToken:pushToken}}},{},function(err){
+              if (err){
+                res.status(500);
+                return next();
+              }else{
+                res.status(201);
+                return next();
+              }
+          });
+        }
+      }else{
+        res.status(401);
+        return next();
+      }
+    });
+    /*
     console.log('Handling registration request...');
     console.log("#<RegistrationRequest device_id: " + req.params.device_id +
       ", pass_type_id: " + req.params.pass_type_id +
@@ -53,7 +85,9 @@ function initServer(server){
       ", authentication_token: " + req.header('Authorization') +
       ", push_token: " + req.params.pushToken+">");
     res.status(200);
+    */
     return next();
+
   });
 
   server.get({path:'/passws/v1/devices/:device_id/registrations/:pass_type_id?'},function (req, res, next){
